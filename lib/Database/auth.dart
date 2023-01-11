f4f059dab0e453f8586426c75eb73231945c035c
Azor <wizfaiz@icloud.com>
2023-01-11 09:44:53 +0100

    adding sign in anonymous authentication function

--- lib/Database/auth.dart
+++ lib/Database/auth.dart
@@ -3,6 +3,11 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 final GoogleSignIn _googlesignin = GoogleSignIn();
 
+final _auth = FirebaseAuth.instance;
+
 class Database {
-  
+  Future signinAnon() async {
+    dynamic user = await _auth.signInAnonymously();
+    return user;
+  }
 }
